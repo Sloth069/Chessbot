a2d771b63427f03dd74a28408905df8e1a207655
Sloth069 <mxa.bechtel@gmail.com>
2024-11-18 12:11:56 +0100

    Delete arithmetic task.py

--- arithmetic task.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    sum1 = int(a) + int(b)
-    print(sum1)
-    sum2 = int(a) - int(b)
-    print(sum2)
-    sum3 = int(a) * int(b)
-    print(sum3)
