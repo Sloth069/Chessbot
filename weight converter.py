fa1c5915acad6468ab1cca24e20164d5b1c9e36c
Sloth069 <mxa.bechtel@gmail.com>
2024-11-18 12:13:16 +0100

    Delete weight converter.py

--- weight converter.py
+++
@@ -1,9 +0,0 @@
-weight = input("weight: ")
-Type = input("(k)g or (L)bs): ")
-
-if Type == "K" :
-    print("Your weight in pounds is", float(weight) * 2.205, "lb")
-else :
-    print("Your weight in kilograms is", float(weight) / 2.205, "kg")
-
-print(type(weight))
